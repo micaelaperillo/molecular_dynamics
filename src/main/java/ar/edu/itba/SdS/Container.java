f26c4b89e37ba0fa7fcf4175157b8fe75b3c1ed5
NCasella <nicolascasella02@gmail.com>
2025-04-11 11:02:55 -0300

    refactor: refactor

--- src/main/java/ar/edu/itba/SdS/Container.java
+++ src/main/java/ar/edu/itba/SdS/Container.java
@@ -7,21 +7,31 @@ import java.util.Random;
 public class Container {
     public static final double CONTAINER_DIAMETER=0.1;
     public static final double OBSTACLE_RADIUS=0.005;
+    public final Particle obstacleParticle;
     private final List<Particle> particles=new ArrayList<>();
     private static final Random rand=new Random();
 
     private final double particlesSpeed,particlesRadius;
     private final int particlesAmount;
-    public Container(int particlesAmount,double particlesSpeed,double particlesRadius){
+    public Container(int particlesAmount,double particlesSpeed,double particlesRadius,boolean obstaclePresent){
         this.particlesAmount=particlesAmount;
         this.particlesSpeed =particlesSpeed;
         this.particlesRadius=particlesRadius;
+        if(!obstaclePresent)
+            this.obstacleParticle=new Particle(OBSTACLE_RADIUS,0,0,0,0,3);
+        else
+            this.obstacleParticle=null;
     }
 
     public void initializeParticles() {
         double minR = OBSTACLE_RADIUS + particlesRadius;
         double maxR = CONTAINER_DIAMETER/2 - particlesRadius;
-        for(int i=0;i<particlesAmount;i++) {
+        int i=0;
+        if(obstacleParticle!=null){
+            particles.add(obstacleParticle);
+            i++;
+        }
+        for(;i<particlesAmount;i++) {
             int maxAttempts = 10000;
             boolean placed = false;
             for (int attempts = 0; attempts < maxAttempts && !placed; attempts++) {
